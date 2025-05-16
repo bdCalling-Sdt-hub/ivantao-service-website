a7bce1e0e65ed2fc81a06550289c1d8c2fc14f5e
KshSiaan <kshsiaan@gmail.com>
2025-05-16 15:01:08 +0600

    base-ur_changed

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 // export const base_url = "http://10.0.80.16:8002/api";
-export const base_url = "http://137.59.180.219:7000/api";
+export const base_url = "http://20.233.235.159/api";
 
 export const defaultUserProfile =
   "https://api.dicebear.com/9.x/big-ears-neutral/svg?eyes=variant03&mouth=variant0702&nose=variant01&backgroundColor=ffffff";
